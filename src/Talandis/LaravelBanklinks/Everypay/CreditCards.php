417d2ed52c304f1fee98f293f79003d6befd30ee
Tomas Talandis <tomas@talandis.lt>
2020-09-22 11:02:11 +0300

    add credit cards payment method for everypay

---
+++ src/Talandis/LaravelBanklinks/Everypay/CreditCards.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Talandis\LaravelBanklinks\Everypay;
+
+use Talandis\LaravelBanklinks\Banklink\Everypay;
+
+class CreditCards extends Everypay {
+
+    protected $bank = 'card';
+
+}
