5ee8e48817d6d02f79779bce1b27fe3b31a1fbfd
Tomas Talandis <tomas@talandis.lt>
2021-02-08 16:35:51 +0200

    add siauliu bankas integration

---
+++ src/Talandis/LaravelBanklinks/Everypay/Lithuania/Siauliu.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Talandis\LaravelBanklinks\Everypay\Lithuania;
+
+use Talandis\LaravelBanklinks\Banklink\Everypay;
+
+class Siauliu extends Everypay {
+
+    protected $bank = 'sb_ob_lt';
+
+}
